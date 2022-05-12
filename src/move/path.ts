985da848795a2f3a7a50bdbead2ed927133e1e6e
xzr <derelix0@outlook.com>
2022-05-12 18:32:32 +0800

    path

---
+++ src/move/path.ts
@@ -0,0 +1,47 @@
+import _ from "lodash";
+
+export const crawlTo = function(creep:Creep, targetPos:RoomPosition){
+    var x=targetPos.x, y=targetPos.y, roomName=targetPos.roomName
+    if(creep.room.name != roomName){
+        creep.say('???')
+        return ERR_INVALID_ARGS
+    }
+    const _move = creep.memory._move;
+    if(!_move || _move.room != roomName || _move.dest != {x,y,room:roomName}){
+        if(seekTo(creep, targetPos) == ERR_NO_PATH)
+            return ERR_NO_PATH
+        if(!_move) return ERR_NO_PATH
+    }
+    
+    const path = _.isString(_move.path) ? Room.deserializePath(_move.path) : _move.path;
+    const idx = _.findIndex(path, {x: creep.pos.x, y: creep.pos.y});
+    if(idx != -1) {
+        path.splice(0,idx+1);
+        try {_move.path = Room.serializePath(path);}
+        catch(e) {console.log('$ERR',creep.pos,x,y,roomName,JSON.stringify(path)); throw e;}
+    }
+    if(path.length == 0) {
+        return creep.pos.isNearTo(targetPos) ? OK : ERR_NO_PATH;
+    }
+    return creep.moveByPath(path);
+}
+
+const seekTo = function(creep:Creep, targetPos:RoomPosition){
+    var x=targetPos.x, y=targetPos.y, roomName=targetPos.roomName
+    delete creep.memory._move;
+    const path = creep.room.findPath(creep.pos, targetPos, {
+        costCallback:function(roomName: string, costMatrix: CostMatrix): void | CostMatrix {
+            return
+        }
+    });
+    if(path.length > 0) {
+        creep.memory._move = {
+            dest: {x,y,room:roomName},
+            time: Game.time,
+            path: Room.serializePath(path),
+            room: creep.pos.roomName
+        };
+        return OK
+    }
+    return ERR_NO_PATH
+}
--- src/move/route.ts
+++ src/move/route.ts
@@ -1,24 +1,27 @@
-import _ from "lodash";
+import { crawlTo } from "./path"
 
 const hikeTo = function(creep:Creep, targetPos:RoomPosition){
     if(creep.room.name == targetPos.roomName)
         return crawlTo(creep,targetPos)
     const _hike = creep.memory._hike
-    if(!_hike || _hike.to != targetPos.roomName){
+    if(!_hike || _hike.to != targetPos.roomName) {
         if(seekTo(creep, targetPos.roomName) == ERR_NO_PATH)
             return ERR_NO_PATH
+        if(!_hike || !_hike.route[0]) return ERR_NO_PATH
     }
-    if(!_hike || !_hike.route[0]) return ERR_NO_PATH
+
+    if(creep.fatigue) return ERR_TIRED
     if(creep.room.name == _hike.from){
         const exit = creep.pos.findClosestByRange(_hike.route[0].exit);
         return exit ? crawlTo(creep,exit) : ERR_NO_PATH
     }
-    if(creep.fatigue == 0 && creep.room.name == _hike.route[0].room){
+    if(creep.room.name == _hike.route[0].room){
         _hike.from = _hike.route[0].room
         _hike.route.shift()
         const exit = creep.pos.findClosestByRange(_hike.route[0].exit);
         return exit ? crawlTo(creep,exit) : ERR_NO_PATH
     }
+    creep.say('...')
     return ERR_TIRED
 }
 
@@ -45,56 +48,4 @@ const routeCallback = function(roomName:string):number {
     } else {
         return 2.5;
     }
-}
-
-const crawlTo = function(creep:Creep,targetPos:RoomPosition){
-    var x=targetPos.x, y=targetPos.y, roomName=targetPos.roomName
-
-    if(creep.memory._move) {
-
-        var _move = creep.memory._move;
-
-        if(Game.time > _move.time + 10 || _move.room != creep.pos.roomName) {
-            delete creep.memory._move;
-        }
-        else if(_move.dest.room == roomName && _move.dest.x == x && _move.dest.y == y) {
-
-            var path = _.isString(_move.path) ? Room.deserializePath(_move.path) : _move.path;
-
-            var idx = _.findIndex(path, {x: creep.pos.x, y: creep.pos.y});
-            if(idx != -1) {
-                var oldMove = _.cloneDeep(_move);
-                path.splice(0,idx+1);
-                try {
-                    _move.path = Room.serializePath(path);
-                }
-                catch(e) {
-                    console.log('$ERR',creep.pos,x,y,roomName,JSON.stringify(path),'-----',JSON.stringify(oldMove));
-                    throw e;
-                }
-            }
-            if(path.length == 0) {
-                return creep.pos.isNearTo(targetPos) ? OK : ERR_NO_PATH;
-            }
-
-            var result = creep.moveByPath(path);
-            if(result == OK) {
-                return OK;
-            }
-        }
-    }
-
-    var path = creep.pos.findPathTo(targetPos);
-    creep.memory._move = {
-        dest: {x,y,room:roomName},
-        time: Game.time,
-        path: Room.serializePath(path),
-        room: creep.pos.roomName
-    };
-
-    if(path.length == 0) {
-        return ERR_NO_PATH;
-    }
-
-    return creep.move(path[0].direction);
 }
