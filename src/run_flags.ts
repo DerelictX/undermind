09af7f97031af706179db2733eaaddcda62f7eea
xzr <derelix0@outlook.com>
2024-04-24 18:34:26 +0800

    线型小队移动

--- src/run_flags.ts
+++ src/run_flags.ts
@@ -12,9 +12,6 @@ export const run_flags = function () {
             if (flag.memory._class == '_loop') {
                 loop_flags(flag)
             }
-            if (flag.memory._class == '_squad') {
-                run_squad(flag)
-            }
         } catch (error) {
             console.log(name + ':\t' + error);
         }
@@ -58,7 +55,7 @@ export const init_room_flag = function (room: Room) {
 }
 _.assign(global, {init_room_flag: init_room_flag})
 
-const run_squad = function (flag: Flag) {
+const run_squad_square = function (flag: Flag) {
     //const offset = (0 << 0) | (1 << 2) | (3 << 4) | (2 << 6)
     const _squad = flag.memory._squad
     if (!_squad) return;
@@ -88,12 +85,57 @@ const run_squad = function (flag: Flag) {
     for (let i = 0; i < squad_length; ++i) {
         const creep = Game.creeps[_squad.member[i]]
         if (!creep) continue
-        const off_xy = (_squad.offset_pos << (2 * i)) & 3
-        const x = _squad.head_pos.x + (off_xy & 1)
-        const y = _squad.head_pos.y + ((off_xy & 2) >> 1)
-
-        const pos_str = base64table[x] + base64table[y]
+        const pos_str = base64table[creep.pos.x] + base64table[creep.pos.x]
         _move_intents[pos_str] = {id: creep.id, step: [step.direction]}
+    }
+    _squad.head_pos.x += step.dx
+    _squad.head_pos.y += step.dy
+    delete _squad.step
+}
+
+const run_squad_line = function (flag: Flag) {
+    //const offset = (0 << 0) | (1 << 2) | (3 << 4) | (2 << 6)
+    const _squad = flag.memory._squad
+    if (!_squad) return;
+    if (!_squad.step) crawlSquad(_squad, flag.pos)
+    const step = _squad.step
+    if (!step) return;
+
+    const roomName = _squad.head_pos.roomName
+    let ready = true
+    const squad_length = _squad.member.length;
+    let prev_creep: RoomPosition | undefined = undefined
+    for (let i = 0; i < squad_length; ++i) {
+        const creep = Game.creeps[_squad.member[i]]
+        if (!creep) continue
+        if (!prev_creep) {
+            if (!creep.pos.isEqualTo(new RoomPosition(_squad.head_pos.x, _squad.head_pos.x, roomName))) {
+                ready = false
+                hikeTo(creep, new RoomPosition(_squad.head_pos.x, _squad.head_pos.x, roomName))
+            }
+        } else {
+            if (!creep.pos.isNearTo(prev_creep)) {
+                ready = false
+                hikeTo(creep, prev_creep)
+            }
+        }
+        prev_creep = creep.pos
+    }
+    if (!ready) return
+
+    /**设置intent */
+    prev_creep = undefined
+    const _move_intents = global._move_intents[roomName] ?? (global._move_intents[roomName] = {})
+    for (let i = 0; i < squad_length; ++i) {
+        const creep = Game.creeps[_squad.member[i]]
+        if (!creep) continue
+        const pos_str = base64table[creep.pos.x] + base64table[creep.pos.x]
+        if (!prev_creep) {
+            _move_intents[pos_str] = {id: creep.id, step: [step.direction]}
+        } else {
+            _move_intents[pos_str] = {id: creep.id, step: [creep.pos.getDirectionTo(prev_creep)]}
+        }
+        prev_creep = creep.pos
     }
     _squad.head_pos.x += step.dx
     _squad.head_pos.y += step.dy
