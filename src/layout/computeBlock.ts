4de313b58e295d0c26192930757a20e05439a5d3
xzr <derelix0@outlook.com>
2024-12-31 17:48:44 +0800

    TwT

--- src/layout/computeBlock.ts
+++ src/layout/computeBlock.ts
@@ -156,6 +156,85 @@ function deleteExitBlock(roomWalkable: CostMatrix, unionFind: UnionFind, sizeMap
     })
 }
 
+function mergeBlock(nd: pos_node & { v: number }, queMin: FlagHeapUtils<pos_node>,
+                    unionFind: UnionFind, sizeMap: Record<number, number>, posSeqMap: Record<number, number[]>, roomWalkable: CostMatrix,
+                    putAbleCacheMap: Record<number, pos_node[]> = {}, allCacheMap: Record<number, pos_node[]> = {}) {
+    const pos = nd.v;
+    if (nd.w != sizeMap[pos]) return;// 已经被合并了
+
+    const visited = new PathFinder.CostMatrix()
+    const nearCntMap: Record<number, number> = {}
+    let allNearCnt = 0
+
+    //搜索附近的块
+    posSeqMap[pos].forEach(e => {
+        const y = e % 50;
+        const x = Math.round((e - y) / 50);//Math.round
+        forNear(roomWalkable, (x, y, val) => {
+            if (val && !exec(visited, x, y, 1)) {
+                let currentPos = unionFind.find(x * 50 + y);
+                if (currentPos == pos) return;
+                allNearCnt += 1
+                let currentSize = sizeMap[currentPos];
+                if (currentSize < 300) {
+                    nearCntMap[currentPos] = (nearCntMap[currentPos] || 0) + 1;
+                }
+            }
+        }, x, y)
+    })
+
+    let targetPos = 0;
+    let nearCnt = 0;
+    let maxRatio = 0;
+
+    // 找出合并附近最优的块
+    for (let currentPos in nearCntMap) {
+        const currentRatio = nearCntMap[currentPos] / Math.sqrt(Math.min(sizeMap[currentPos], nd.w))//实际/期望
+        if (currentRatio == maxRatio ? sizeMap[currentPos] < sizeMap[targetPos] : currentRatio > maxRatio) {
+            targetPos = Number(currentPos);
+            maxRatio = currentRatio;
+            nearCnt = nearCntMap[currentPos];
+        }
+    }
+    for (let currentPos in nearCntMap) {
+        if (nearCnt < nearCntMap[currentPos]) {
+            targetPos = Number(currentPos);
+            nearCnt = nearCntMap[currentPos];
+        }
+    }
+
+    const minSize = sizeMap[targetPos];
+    const cnt = nd.w + minSize;
+    // let nearRatio =nearCntMap[targetPos]/allNearCnt;
+
+    const minPlaneCnt = 140
+    let targetBlockPutAbleCnt = 0
+    let ndkBlockPutAbleCnt = 0
+    if (minSize > minPlaneCnt)
+        targetBlockPutAbleCnt = getBlockPutAbleCnt(roomWalkable, visited, queMin, unionFind, targetPos, putAbleCacheMap, allCacheMap)[0].length
+    if (nd.w > minPlaneCnt)
+        ndkBlockPutAbleCnt = getBlockPutAbleCnt(roomWalkable, visited, queMin, unionFind, nd.v, putAbleCacheMap, allCacheMap)[0].length
+
+    // 合并
+    if (targetPos && Math.max(targetBlockPutAbleCnt, ndkBlockPutAbleCnt) < minPlaneCnt) {//&&(maxRatio-Math.sqrt(cnt)/20>=0||(nearRatio>0.7&&nd.k<100))
+        // if(targetPos&&(cnt<300||Math.min(nd.k,minSize)<150)&&(maxRatio-Math.sqrt(cnt)/20>=0||Math.max(nd.k,minSize)<200||(nearRatio>0.7&&nd.k<100))){//*Math.sqrt(nearRatio)
+
+        unionFind.union(pos, targetPos);
+        nd.v = unionFind.find(pos);
+
+        if (pos != nd.v) delete sizeMap[pos];
+        else delete sizeMap[targetPos];
+
+        nd.w = cnt;
+        sizeMap[nd.v] = cnt;
+        posSeqMap[nd.v] = posSeqMap[targetPos].concat(posSeqMap[pos])
+        delete putAbleCacheMap[nd.v]
+        delete putAbleCacheMap[targetPos]
+        if (pos != nd.v) delete posSeqMap[pos];
+        else delete posSeqMap[targetPos];
+        return {w: nd.w, x: nd.x, y: nd.y, v: nd.v};
+    }
+}
 
 /**
  * 插值，计算区块的预处理和合并需求
@@ -174,7 +253,7 @@ function computeBlock(roomName: string) {
 
     // 分块，将地图分成一小块一小块
     const unionFind = new UnionFind(50 * 50)
-    let visited = new PathFinder.CostMatrix()
+    const visited = new PathFinder.CostMatrix()
     const queMinBlock = new FlagHeapUtils<pos_node & { v: number }>()
     for (let i = 0; i < 3000; i++) {
         if (makeBlock(startPoint, queMinBlock, visited, nearWallWithInterpolation, unionFind, sizeMap, posSeqMap))
@@ -184,91 +263,17 @@ function computeBlock(roomName: string) {
 
     let putAbleCacheMap: Record<number, pos_node[]> = {}
     let allCacheMap: Record<number, pos_node[]> = {}
-    // let i = 0
+
     // 合并小块成大块的
-    const queMin2 = new FlagHeapUtils<pos_node>()
+    const queMin = new FlagHeapUtils<pos_node>()
     queMinBlock.whileNoEmpty(nd => {
-        let pos = nd.v;
-        if (nd.w != sizeMap[pos]) return;// 已经被合并了
-        // i++;
-
-        visited = new PathFinder.CostMatrix()
-        let nearCntMap: Record<number, number> = {}
-        let allNearCnt = 0
-
-        //搜索附近的块
-        posSeqMap[pos].forEach(e => {
-            let y = e % 50;
-            let x = Math.round((e - y) / 50);//Math.round
-            forNear(roomWalkable, (x, y, val) => {
-                if (val && !exec(visited, x, y, 1)) {
-                    let currentPos = unionFind.find(x * 50 + y);
-                    if (currentPos == pos) return;
-                    allNearCnt += 1
-                    // if(i==104)
-                    // visual.text(parseInt(1*10)/10, x,y+0.25, {color: "cyan",opacity:0.99,font: 7})
-                    let currentSize = sizeMap[currentPos];
-                    if (currentSize < 300) {
-                        nearCntMap[currentPos] = (nearCntMap[currentPos] || 0) + 1;
-                    }
-                }
-            }, x, y)
-        })
-
-        let targetPos = 0;
-        let nearCnt = 0;
-        let maxRatio = 0;
-
-        // 找出合并附近最优的块
-        for (let currentPos in nearCntMap) {
-            let currentRatio = nearCntMap[currentPos] / Math.sqrt(Math.min(sizeMap[currentPos], nd.w))//实际/期望
-            if (currentRatio == maxRatio ? sizeMap[currentPos] < sizeMap[targetPos] : currentRatio > maxRatio) {
-                targetPos = Number(currentPos);
-                maxRatio = currentRatio;
-                nearCnt = nearCntMap[currentPos];
-            }
-        }
-        for (let currentPos in nearCntMap) {
-            if (nearCnt < nearCntMap[currentPos]) {
-                targetPos = Number(currentPos);
-                nearCnt = nearCntMap[currentPos];
-            }
-        }
-
-        let minSize = sizeMap[targetPos];
-        let cnt = nd.w + minSize;
-        // let nearRatio =nearCntMap[targetPos]/allNearCnt;
-
-        const minPlaneCnt = 140
-        let targetBlockPutAbleCnt = 0
-        let ndkBlockPutAbleCnt = 0
-        if (minSize > minPlaneCnt)
-            targetBlockPutAbleCnt = getBlockPutAbleCnt(roomWalkable, visited, queMin2, unionFind, targetPos, putAbleCacheMap, allCacheMap)[0].length
-        if (nd.w > minPlaneCnt)
-            ndkBlockPutAbleCnt = getBlockPutAbleCnt(roomWalkable, visited, queMin2, unionFind, nd.v, putAbleCacheMap, allCacheMap)[0].length
-
-        // 合并
-        if (targetPos && Math.max(targetBlockPutAbleCnt, ndkBlockPutAbleCnt) < minPlaneCnt) {//&&(maxRatio-Math.sqrt(cnt)/20>=0||(nearRatio>0.7&&nd.k<100))
-            // if(targetPos&&(cnt<300||Math.min(nd.k,minSize)<150)&&(maxRatio-Math.sqrt(cnt)/20>=0||Math.max(nd.k,minSize)<200||(nearRatio>0.7&&nd.k<100))){//*Math.sqrt(nearRatio)
-
-            unionFind.union(pos, targetPos);
-            nd.v = unionFind.find(pos);
-
-            if (pos != nd.v) delete sizeMap[pos];
-            else delete sizeMap[targetPos];
-
-            nd.w = cnt;
-            sizeMap[nd.v] = cnt;
-            posSeqMap[nd.v] = posSeqMap[targetPos].concat(posSeqMap[pos])
-            delete putAbleCacheMap[nd.v]
-            delete putAbleCacheMap[targetPos]
-            if (pos != nd.v) delete posSeqMap[pos];
-            else delete posSeqMap[targetPos];
-            queMinBlock.push({w: nd.w, x: nd.x, y: nd.y, v: nd.v});
-        }
+        const newNode: undefined | pos_node & { v: number }
+            = mergeBlock(nd, queMin, unionFind, sizeMap, posSeqMap, roomWalkable, putAbleCacheMap, allCacheMap)
+        if (newNode) queMinBlock.push(newNode);
     })
     global.layout = {
-        unionFind: unionFind, sizeMap: sizeMap, roomWalkable: roomWalkable,
-        nearWall: nearWall, putAbleCacheMap: putAbleCacheMap, allCacheMap: allCacheMap
+        unionFind: unionFind, sizeMap: sizeMap,
+        roomWalkable: roomWalkable, nearWall: nearWall,
+        putAbleCacheMap: putAbleCacheMap, allCacheMap: allCacheMap
     }
 }
